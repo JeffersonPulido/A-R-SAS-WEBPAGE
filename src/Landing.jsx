4dc395a8c03b139336a0ec6b8a8e23ea34d5be71
JeffersonPulido <jeffersonpulido55@gmail.com>
2023-03-07 08:43:39 -0500

    first commit

---
+++ src/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navbar } from './components/navbar/Navbar'
+import { Footer } from './components/footer/Footer'
+import { Header } from './components/header/Header'
+import { Services } from './components/services/Services'
+import { Testimonials } from './components/testimonials/Testimonials'
+import { Copyright } from './components/copyright/Copyright'
+import { Whatsapp } from './components/btnWhatsapp/Whatsapp'
+
+export const Landing = () => {
+  return (
+    <>
+      <Navbar/>
+
+    </>
+  )
+}
